7556cd65bc3b79a399c8886d00bfe5f93687b0e7
erudmila <144694341+erudmila@users.noreply.github.com>
2024-03-21 12:25:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Program to calculate the area of a rectangle
+
+# Get input for height & width. Convert to integers and store in variables
+height = int(input("Enter the height of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+# Calculate the area & store the result in the area variable
+area = height * width
+# Output the area variable (converted to string)
+print("The area of the rectangle is " + str(area)) 
